bd7057b5b3dcb8145d661a36dfe1a39db27e4a0c
phermes <phermes@cern.ch>
2017-08-25 11:32:49 +0200

    new and faster loss map plotting

--- postprocessing/lossmap.py
+++ postprocessing/lossmap.py
@@ -1,17 +1,54 @@
-### INITIALIZATION
-
+import pandas as pd
 import numpy as np
-import sys
 import matplotlib.pyplot as plt
-from optparse import OptionParser
-import os
+import sys
 
-from hisix import *
-import plottingtools as pt
-import lhcTools as lt
 
-path = {}
-beam = 1
+WarmRegions=np.array([[  0.00000000e+00,   2.25365000e+01],
+       [  5.48530000e+01,   1.52489000e+02],
+       [  1.72165500e+02,   1.92400000e+02],
+       [  1.99484700e+02,   2.24300000e+02],
+       [  3.09545428e+03,   3.15562858e+03],
+       [  3.16774008e+03,   3.18843308e+03],
+       [  3.21144458e+03,   3.26386758e+03],
+       [  3.30990008e+03,   3.35497408e+03],
+       [  3.40100558e+03,   3.45342858e+03],
+       [  3.47644008e+03,   3.49406558e+03],
+       [  3.50588528e+03,   3.56831858e+03],
+       [  6.40540880e+03,   6.45791380e+03],
+       [  6.46877850e+03,   6.85951380e+03],
+       [  6.87037850e+03,   6.92353380e+03],
+       [  9.73590702e+03,   9.82473052e+03],
+       [  9.83083202e+03,   9.86173052e+03],
+       [  9.87873202e+03,   9.93998552e+03],
+       [  9.95054802e+03,   1.00434620e+04],
+       [  1.00540245e+04,   1.01152780e+04],
+       [  1.01322795e+04,   1.01639705e+04],
+       [  1.01700720e+04,   1.02576030e+04],
+       [  1.31049892e+04,   1.31298045e+04],
+       [  1.31368892e+04,   1.31571237e+04],
+       [  1.31768002e+04,   1.32716472e+04],
+       [  1.33067527e+04,   1.33518257e+04],
+       [  1.33869312e+04,   1.34817782e+04],
+       [  1.35014547e+04,   1.35227845e+04],
+       [  1.35298692e+04,   1.35546845e+04],
+       [  1.63946378e+04,   1.64508713e+04],
+       [  1.64569728e+04,   1.64872713e+04],
+       [  1.64933728e+04,   1.68308713e+04],
+       [  1.68369728e+04,   1.68672713e+04],
+       [  1.68733728e+04,   1.69282948e+04],
+       [  1.97740644e+04,   2.02179087e+04],
+       [  2.30899797e+04,   2.31385770e+04],
+       [  2.31503967e+04,   2.31713755e+04],
+       [  2.31943870e+04,   2.32468100e+04],
+       [  2.32928425e+04,   2.33379155e+04],
+       [  2.33839480e+04,   2.34363710e+04],
+       [  2.34593825e+04,   2.34800825e+04],
+       [  2.34921940e+04,   2.35531160e+04],
+       [  2.64334879e+04,   2.64583032e+04],
+       [  2.64653879e+04,   2.64867177e+04],
+       [  2.65063942e+04,   2.66012412e+04],
+       [  2.66363467e+04,   2.66588832e+04]])
 
 
 ### CHECK ARGUMENTS
@@ -19,95 +56,49 @@ beam = 1
 nargs = len(sys.argv)
 
 if nargs<2:
-    print 'ERROR: no hisix directory given; ABORT'
+    print('ERROR: no hisix directory given; ABORT')
     exit()
 elif nargs == 2:
-    path[1] = sys.argv[1]
-elif nargs >2:
-    print 'Reading multiple directories'
-    for j in range(1,nargs):
-        path[j] = sys.argv[j]
+    path = str(sys.argv[1])
 
+print(path)
+    
 
+def assign_warm_cold(s):
+    for i in WarmRegions:
+        if (s>i[0]) and (s<i[1]):
+            return 'red'
+    return 'blue'
 
-### LOAD THE OBJECTS TO PROCESS THE LOSSES
+class plot_hisix:
+    def __init__(self,path):
+        self.path = path
+        self.read_collimator_losses()
+        self.read_aperture_losses()
+        self.plot_losses()
+    def read_collimator_losses(self):
+        self.coll = pd.read_csv('{0}/summary/fort.208'.format(self.path),
+                    delim_whitespace=True,names=['name','s','colid','energy'])
+        self.norm = self.coll['energy'].max()
+        
+    def read_aperture_losses(self):
+        self.aper = pd.read_csv('{0}/summary/fort.999'.format(self.path),
+                    delim_whitespace=True,usecols=[0,1],names=['s','energy'])
+        self.aper = self.aper.assign(temp=self.aper['s'].apply(assign_warm_cold))
+        
+    def plot_losses(self):
+        plt.bar(self.coll['s'],self.coll['energy']/self.norm,edgecolor='k',width=1)
 
-f3pos  = ['../../fort.3', '../../input/fort.3',
-          path[1]+'../fort.3']
-twpos  = ['../../preprocessing/optics/twiss_b1.tfs', 
-          '../../input/twiss_b1.tfs',
-          path[1]+'../twiss_b1.tfs']
+        for c in ['blue','red']:
+            _data = self.aper[self.aper['temp']==c]
+            plt.bar(_data['s'].values,_data['energy'].values/(self.norm*0.1),edgecolor=c,width=0.1)
 
+        plt.yscale('log')
+        plt.grid(axis='hor')
+        plt.xlabel('Position [m]')
+        plt.ylabel(r'$\eta [1/m]$')
+        plt.grid(axis='y')
+        plt.show()
 
-# check where the fort.3 is located
-
-for p in f3pos:
-    if os.path.isfile(p):
-        fort3 = p
-        break
-
-# check where the twiss file is located
-
-for p in twpos:
-    if os.path.isfile(p):
-        twiss = p
-        break
-
-
-
-#### READ THE LOSSES FROM THE SIMULATION
-
-losses = losses(fort3,twiss)
-
-
-### READ THE SUMMARY FILES
-
-for k in path.keys():
-    losses.getSummary(path[k])
-losses.finalize()
-
-
-
-
-# cycle for beam 2
-
-if beam == 2:
-    losses.cbin=(losses.cbin-lt.clhc)*-1
-    losses.wbin=(losses.wbin-lt.clhc)*-1
-    for k in range(len(losses.collimator_array)):
-        losses.collimator_array[k][0]=(losses.collimator_array[k][0]-lt.clhc)*(-1)
-
-
-
-
-### PLOT THE LOSSES
-
-xmin,xmax = 0,lt.clhc
-ymin,ymax = 1e-6,3
-
-nbins     = int(lt.clhc/0.10)
-width     = 0.80*lt.clhc/nbins
-
-fig, (ax0) = plt.subplots()
-
-
-ax0.bar(losses.cbin,losses.cold/(0.1*losses.normfac),width=width, 
-        color='blue', edgecolor='blue', label='Cold')
-ax0.bar(losses.wbin,losses.warm/(0.1*losses.normfac),width=width, 
-        color='red', edgecolor='red', label='Warm')
-ax0.bar(losses.collimator_array[:,0],losses.collimator_array[:,1]/losses.normfac,width=1.0,color='black',edgecolor='black',label='Collimator')
-
-
-## PLOT PROPERTIES
-
-ax0.set_yscale('log', nonposy='clip')
-ax0.set_xlim(xmin,xmax)
-ax0.set_ylim(ymin,ymax)
-ax0.set_xlabel(r'Longitudinal Coordinate (m)')
-ax0.set_ylabel(r'Local cleaning inefficiency $\eta$ (1/m)')
-ax0.xaxis.grid(True)
-ax0.yaxis.grid(False)
-ax0.grid()
-ax0.legend(loc=0)
-
-plt.show()
+        
+plot_hisix(path)
